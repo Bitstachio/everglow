d6410e03a1a6be47833b19bbe09f42604445318f
BarbodH <habibi.k.barbod@gmail.com>
2026-02-01 00:02:17 -0500

    Set up basic user resource

---
+++ api/src/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+}
--- api/src/users/entities/user.entity.ts
+++ api/src/users/entities/user.entity.ts
@@ -1,7 +1,6 @@
 import { STRING_LIMITS } from "src/common/constants/schema.constants";
-import { Column, Entity, OneToOne } from "typeorm";
+import { Column, Entity } from "typeorm";
 import { BaseEntity } from "../../common/entities/base.entity";
-import { UserAuth } from "./user-auth.entity";
 
 @Entity("users")
 export class User extends BaseEntity {
@@ -10,7 +9,4 @@ export class User extends BaseEntity {
 
   @Column({ type: "varchar", length: STRING_LIMITS.STANDARD })
   name: string;
-
-  @OneToOne(() => UserAuth, (userAuth) => userAuth.user, { cascade: true })
-  auth: UserAuth;
 }
