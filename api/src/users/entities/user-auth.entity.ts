6cd70c4acb1cc998ecaa00bf796102decf1fcc00
BarbodH <habibi.k.barbod@gmail.com>
2026-01-31 20:40:11 -0500

    Polish user auth entity

--- api/src/users/entities/user-auth.entity.ts
+++ api/src/users/entities/user-auth.entity.ts
@@ -1,34 +1,20 @@
-import * as bcrypt from "bcrypt";
-import { BeforeInsert, BeforeUpdate, Column, Entity, JoinColumn, OneToOne } from "typeorm";
+import { STRING_LIMITS } from "src/common/constants/schema.constants";
+import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
 import { BaseEntity } from "../../common/entities/base.entity";
 import { User } from "./user.entity";
-import { STRING_LIMITS } from "src/common/constants/schema.constants";
 
 @Entity("user_auth")
 export class UserAuth extends BaseEntity {
   @Column({ name: "user_id", unique: true })
   userId: string;
 
-  @Column({ name: "password_hash", type: "varchar", length: STRING_LIMITS.STANDARD })
+  @Column({ name: "password_hash", length: STRING_LIMITS.STANDARD })
   passwordHash: string;
 
-  @Column({ name: "refresh_token", type: "text", nullable: true })
-  refreshToken: string | null;
+  @Column({ name: "refresh_token_hash", length: STRING_LIMITS.STANDARD, nullable: true })
+  refreshTokenHash: string | null;
 
   @OneToOne(() => User, (user) => user.auth, { onDelete: "CASCADE" })
   @JoinColumn({ name: "user_id" })
   user: User;
-
-  @BeforeInsert()
-  @BeforeUpdate()
-  async hashPassword() {
-    if (this.passwordHash && !this.passwordHash.startsWith("$2")) {
-      const salt = await bcrypt.genSalt(10);
-      this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
-    }
-  }
-
-  async comparePassword(candidatePassword: string): Promise<boolean> {
-    return bcrypt.compare(candidatePassword, this.passwordHash);
-  }
 }
