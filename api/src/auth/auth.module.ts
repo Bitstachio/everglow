c8a015c1e80395e3468bd2482b31741be8151fe7
BarbodH <habibi.k.barbod@gmail.com>
2026-06-08 01:57:44 -0400

    resolve user with provider sub

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
+import { UsersModule } from "src/users/users.module";
 import { JwtStrategy } from "./auth.strategy";
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, UsersModule],
   providers: [JwtStrategy],
 })
 export class AuthModule {}
--- api/src/auth/auth.strategy.ts
+++ api/src/auth/auth.strategy.ts
@@ -1,12 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { JwtPayloadDto } from "./dto";
+import { UsersService } from "src/users/users.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private usersService: UsersService,
+  ) {
     const secret = configService.get<string>("jwt.secret");
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -16,7 +20,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayloadDto) {
-    // TODO: Use user service to fetch user and include ID in return object
-    return payload;
+    const user = await this.usersService.resolveByProviderSub(payload.sub);
+    if (!user) throw new UnauthorizedException("No user found for this identity");
+    return { userId: user.id, providerSub: payload.sub, email: user.email };
   }
 }
