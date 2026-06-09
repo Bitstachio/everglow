f2a33b189f5c1b1ee727daea0c41599347e9ced9
BarbodH <habibi.k.barbod@gmail.com>
2026-06-09 03:26:58 -0400

    set up migration to add user details

---
+++ api/prisma/migrations/20260609072602_add_user_details/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX `User_email_key` ON `User`;
+
+-- AlterTable
+ALTER TABLE `User` DROP COLUMN `email`,
+    DROP COLUMN `name`;
+
+-- CreateTable
+CREATE TABLE `UserDetails` (
+    `id` CHAR(36) NOT NULL,
+    `userId` CHAR(36) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `UserDetails_userId_key`(`userId`),
+    UNIQUE INDEX `UserDetails_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `UserDetails` ADD CONSTRAINT `UserDetails_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
