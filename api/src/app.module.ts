0bcce9e01a8ef80b4565c9e19dac4edb090f0dc4
BarbodH <habibi.k.barbod@gmail.com>
2026-06-16 10:11:43 -0400

    set up Pino with default HTTP access logging

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { LoggerModule } from "nestjs-pino";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { CaslModule } from "./casl/casl.module";
 import auth0Config from "./config/auth0.config";
 import awsConfig from "./config/aws.config";
 import encryptionConfig from "./config/encryption.config";
-import { AuthModule } from "./auth/auth.module";
-import { UsersModule } from "./users/users.module";
-import { CaslModule } from "./casl/casl.module";
 import { PrismaModule } from "./prisma/prisma.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { PrismaModule } from "./prisma/prisma.module";
     UsersModule,
     CaslModule,
     PrismaModule,
+    LoggerModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
