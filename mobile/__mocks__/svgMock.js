0a37fb03b4be1fa2dde54afb939654a9fa084a10
BarbodH <habibi.k.barbod@gmail.com>
2026-09-19 22:28:33 -0400

    set up default AppIcon

--- mobile/__mocks__/svgMock.js
+++ mobile/__mocks__/svgMock.js
@@ -1,1 +1,9 @@
-module.exports = "SvgMock";
+const React = require("react");
+const { View } = require("react-native");
+
+function SvgMock(props) {
+  return React.createElement(View, { testID: "svg-mock", ...props });
+}
+
+module.exports = SvgMock;
+module.exports.default = SvgMock;
