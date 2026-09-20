211c113c3d62ccc4a19e7af2c0852bb498192e4f
BarbodH <habibi.k.barbod@gmail.com>
2026-09-19 22:08:08 -0400

    set up SVG transformer and SVGR

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,4 +3,16 @@ const { withNativewind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+const { transformer, resolver } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+};
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"],
+};
+
 module.exports = withNativewind(config);
